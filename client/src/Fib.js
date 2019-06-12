3e2ee0690ab89f971b94d9f4bb71f846898d7be9
Sam Ober <ober.sam@gmail.com>
2019-06-12 16:11:27 -0400

    Initial commit

---
+++ client/src/Fib.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Fib extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      seenIndexes: [],
+      values: {},
+      index: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    const type = typeof values.data;
+    if (type === 'object' && !!values.data)
+      this.setState({ values: values.data });
+  }
+
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    if (seenIndexes.data.constructor === Array)
+      this.setState({ seenIndexes: seenIndexes.data });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+    this.setState({ index: '' });
+  };
+
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+  }
+
+  renderValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}
+        </div>
+      );
+    }
+
+    return entries;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            type="text"
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
+
+        <h3>Calculated values:</h3>
+        {this.renderValues()}
+      </div>
+    );
+  }
+
+}
+
+export default Fib;
