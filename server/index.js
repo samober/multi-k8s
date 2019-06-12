400116dfe23644f137d18fd0b2aad5a457a87c17
Sam Ober <ober.sam@gmail.com>
2019-06-12 19:31:13 -0400

    some fixes

--- server/index.js
+++ server/index.js
@@ -38,12 +38,16 @@ app.get('/', (req, res) => {
   res.send('Hi');
 });
 
-app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * FROM values');
-  res.send(values.rows);
+app.get('/values/all', async (req, res, next) => {
+  try {
+    const values = await pgClient.query('SELECT * FROM values');
+    res.send(values.rows);
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/values/current', (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
@@ -58,7 +62,12 @@ app.post('/values', async (req, res) => {
 
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  try {
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+  } catch (err) {
+    return next(err);
+  }
 
   res.send({ working: true });
 });
